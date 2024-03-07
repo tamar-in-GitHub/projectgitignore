916d1e11fbd147e5bcd163fa7ab63d796cde2c79
Tamar <T0533169540@gmail.com>
2024-03-07 21:46:37 +0200

    first

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gitIgnore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world first gitIgnore!");
+        }
+    }
+}
